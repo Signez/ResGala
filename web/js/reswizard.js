5364f76c95024c46a468849fb17431b41896128e
Stanislas Signoud <signez@stanisoft.net>
2010-11-29 22:21:39 +0100

    BUGFIX: Le champ « Nom de la banque » s'affiche alors qu'il n'est utile pour les paiements en espèce (merci Dadou).

--- web/js/reswizard.js
+++ web/js/reswizard.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
+  // =====================================================
+  // Étape 2 - Ajout des produits
+  // =====================================================
+
   $(".plhowojs").remove();
-  function updatecommandeprice(){
+  function updateCommandePrice(){
     var total = 0;
 
     // On boucle sur tous les inputs
@@ -18,7 +22,36 @@ $(document).ready(function(){
     else
       $(".wrapnext input").attr("disabled", "");
   }
-  if($(".wrapcommande").length > 0)
-    updatecommandeprice();
-  $(".wrapfield input").change(updatecommandeprice);
+
+  if($(".wrapcommande").length > 0){
+    updateCommandePrice();
+    $(".wrapcommande .wrapfield input").change(updateCommandePrice);
+  }
+
+  // =====================================================
+  // Étape 3 - Paiement et récaptiulatif
+  // =====================================================
+
+  function updateAfficheNomBanque(){
+    if(!$("#paywitesp").is(":checked") && !$("#paywitchk").is(":checked"))
+      $(".wrapnext input").attr("disabled", "disabled");
+    else
+      $(".wrapnext input").attr("disabled", "");
+
+    if($("#paywitchk").is(":checked")){
+      $(".wrapfield:has(#reservation_banque_nom)").stop(true, true)
+        .animate({height: "show", opacity: "show", marginTop: "1em", marginBottom: "1em"});
+    }else{
+      $(".wrapfield:has(#reservation_banque_nom)")
+        .stop(true, true).animate({height: "hide", opacity: "hide", marginTop: 0, marginBottom: 0});
+    }
+  }
+
+  if($("#paywitchk").length > 0){
+    if(!$("#paywitchk").is(":checked")){
+      $(".wrapfield:has(#reservation_banque_nom)").hide();
+    }
+    updateAfficheNomBanque();
+    $(".wrapchoices input").change(updateAfficheNomBanque);
+  }
 });
