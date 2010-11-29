7d84a0d29efe6c1fe44a81f0cda63bc748e38cdf
Stanislas Signoud <signez@stanisoft.net>
2010-11-29 22:05:51 +0100

    BUGFIX: Possibilité de valider une réservation sans aucun produit commandé (merci Dadou).

--- web/js/reswizard.js
+++ web/js/reswizard.js
@@ -2,11 +2,21 @@ $(document).ready(function(){
   $(".plhowojs").remove();
   function updatecommandeprice(){
     var total = 0;
+
+    // On boucle sur tous les inputs
     $(".wrapfield input").each(function(){
       if($(this).val())
         total += parseInt($(this).val()) * parseFloat($(this).parent().attr('price'));
     });
+
+    // On affiche le sous-total informatif
     $(".soustotal strong").text(String(total.toFixed(2)) + " \u20ac");
+
+    // Si le total est nul, on ne peut continuer l'assitant (hey oh, quand même)
+    if(total == 0)
+      $(".wrapnext input").attr("disabled", "disabled");
+    else
+      $(".wrapnext input").attr("disabled", "");
   }
   if($(".wrapcommande").length > 0)
     updatecommandeprice();
