ba950194a63fb455a85238923beb3f4403040eb7
Stanislas Signoud <signez@stanisoft.net>
2010-11-29 23:44:29 +0100

    Ajout de l'information quand à la disponibilité des places.

--- apps/frontoffice/modules/reswizard/templates/_infocheques.php
+++ apps/frontoffice/modules/reswizard/templates/_infocheques.php
@@ -7,3 +7,7 @@
       <p style="color: #990033; font-weight: bold;">Faites attention à l'ordre du chèque et à la date (en cette fin ou début d'année), ça semble évident
         mais il y a encore trop d'erreurs de ce type chaque année !</p>
     </div>
+    <div class="anoter">
+      <p>Il sera possible de retirer ses places à partir du <em>7 février 2011</em> au BdE de <em>12h à 14h</em>, et le <em>12 février 2011</em>
+        de <em>9h à 17h</em>.</p>
+    </div>
